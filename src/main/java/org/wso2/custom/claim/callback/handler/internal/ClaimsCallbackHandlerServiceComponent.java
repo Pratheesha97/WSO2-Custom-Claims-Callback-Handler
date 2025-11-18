3e17cdc3ed845c388407d3432c913eb2700fed2b
Pratheesha97 <samadhipratheesha@gmail.com>
2025-11-18 14:29:45 +0530

    Update ClaimsCallbackHandlerServiceComponent

--- src/main/java/org/wso2/custom/claim/callback/handler/internal/ClaimsCallbackHandlerServiceComponent.java
+++ src/main/java/org/wso2/custom/claim/callback/handler/internal/ClaimsCallbackHandlerServiceComponent.java
@@ -3,28 +3,40 @@ package org.wso2.custom.claim.callback.handler.internal;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleContext;
-import org.osgi.service.component.annotations.*;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.ComponentContext;
 import org.wso2.custom.claim.callback.handler.ClaimsCallbackHandler;
 
 @Component(
-        name = "identity.handler",
-        immediate = true
-)
+        name = "org.wso2.custom.claim.callback.component",
+        immediate = true)
 public class ClaimsCallbackHandlerServiceComponent {
-
     private static final Log log = LogFactory.getLog(ClaimsCallbackHandlerServiceComponent.class);
+    private ServiceRegistration<?> registration;
 
     @Activate
     protected void activate(ComponentContext componentContext) {
-
         try {
             BundleContext bundleContext = componentContext.getBundleContext();
-            bundleContext.registerService(ClaimsCallbackHandler.class.getName(),
-                    new ClaimsCallbackHandler(), null);
-            log.info("ClaimsCallbackHandler handler activated.");
+            registration = bundleContext.registerService(
+                    ClaimsCallbackHandler.class.getName(),
+                    new ClaimsCallbackHandler(),
+                    null
+            );
+            log.info("CustomClaimsCallbackHandler registered successfully.");
         } catch (Throwable e) {
-            log.error("Error while activating id-token-custom-claims.", e);
+            log.error("Error while registering CustomClaimsCallbackHandler.", e);
+        }
+    }
+
+    @Deactivate
+    protected void deactivate(ComponentContext componentContext) {
+        if (registration != null) {
+            registration.unregister();
+            log.info("CustomClaimsCallbackHandler service unregistered successfully.");
         }
     }
 
