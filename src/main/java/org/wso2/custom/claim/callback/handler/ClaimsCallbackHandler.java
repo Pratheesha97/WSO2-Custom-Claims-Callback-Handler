5f239db791da722fe23a4d7c3ddf6f26cfe5cac7
Pratheesha97 <samadhipratheesha@gmail.com>
2025-11-23 13:54:59 +0530

    Update ClaimsCallbackHandler

--- src/main/java/org/wso2/custom/claim/callback/handler/ClaimsCallbackHandler.java
+++ src/main/java/org/wso2/custom/claim/callback/handler/ClaimsCallbackHandler.java
@@ -1,7 +1,6 @@
 package org.wso2.custom.claim.callback.handler;
 
 import com.nimbusds.jwt.JWTClaimsSet;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
@@ -10,10 +9,7 @@ import org.wso2.carbon.identity.oauth2.token.OAuthTokenReqMessageContext;
 import org.wso2.carbon.identity.openidconnect.CustomClaimsCallbackHandler;
 import org.wso2.carbon.identity.openidconnect.DefaultOIDCClaimsCallbackHandler;
 
-import javax.servlet.http.HttpServletRequestWrapper;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -30,21 +26,14 @@ public class ClaimsCallbackHandler extends DefaultOIDCClaimsCallbackHandler impl
             log.debug("Handling custom claims in OAuth token request.");
         }
 
-        JWTClaimsSet jwtClaimsSet = super.handleCustomClaims(builder, request);
-        Map<String, Object> claims = jwtClaimsSet.getClaims();
-
-        List<String> exemptClaimKeys = Arrays.asList("iss", "sub", "azp", "iat", "jti", "nbf", "client_id", "scope", "aut", "exp", "aud");
-
-        for (Map.Entry<String, Object> claimEntry : claims.entrySet()) {
-            if (!exemptClaimKeys.contains(claimEntry.getKey())) {
-                String claimValue = claimEntry.getValue().toString();
-
-                    String encryptedClaimValue = "abc";
-                    builder.claim(claimEntry.getKey(), encryptedClaimValue);
-            }
+        super.handleCustomClaims(builder, request);
+        Map<String, Object> claims = createJWTClaimSet(null, request);
+        for (Map.Entry<String, Object> entry : claims.entrySet()) {
+            builder.claim(entry.getKey(), entry.getValue());
         }
 
         return builder.build();
+
     }
 
 
@@ -55,71 +44,30 @@ public class ClaimsCallbackHandler extends DefaultOIDCClaimsCallbackHandler impl
             log.debug("Handling custom claims in Authorization request.");
         }
 
-        JWTClaimsSet jwtClaimsSet = super.handleCustomClaims(builder, request);
-        Map<String, Object> claims = jwtClaimsSet.getClaims();
-
-        List<String> exemptClaimKeys = Arrays.asList("iss", "sub", "azp", "iat", "jti", "nbf", "client_id", "scope", "aut", "exp", "aud");
-
-        for (Map.Entry<String, Object> claimEntry : claims.entrySet()) {
-            if (!exemptClaimKeys.contains(claimEntry.getKey())) {
-                String claimValue = claimEntry.getValue().toString();
-                    String encryptedClaimValue = "abc";
-                    builder.claim(claimEntry.getKey(), encryptedClaimValue);
-            }
+        super.handleCustomClaims(builder, request);
+        Map<String, Object> claims = createJWTClaimSet(request, null);
+        for (Map.Entry<String, Object> entry : claims.entrySet()) {
+            builder.claim(entry.getKey(), entry.getValue());
         }
-
         return builder.build();
+
     }
 
-    private Map<String, Object> createJWTClaimSet(final OAuthAuthzReqMessageContext authAuthzReqMessageContext,
-                                                  final OAuthTokenReqMessageContext tokenReqMessageContext)
+    private Map<String, Object> createJWTClaimSet(OAuthAuthzReqMessageContext authAuthzReqMessageContext,
+                                                  OAuthTokenReqMessageContext tokenReqMessageContext)
             throws IdentityOAuth2Exception {
 
-        final Map<String, Object> returnObject = new HashMap<>();
+        Map<String, Object> returnObject = new HashMap<>();
 
-        try {
-            final HttpServletRequestWrapper requestWrapper;
-
-            if (tokenReqMessageContext != null) {
-                // Extracting URL parameters in the query string from the request to the token endpoint
-                // e.g., https://localhost:9443/oauth2/token?param1=value1&param2=value2 -> {param1=value1, param2=value2}
-                requestWrapper = tokenReqMessageContext.getOauth2AccessTokenReqDTO().getHttpServletRequestWrapper();
-            } else if (authAuthzReqMessageContext != null) {
-                // Extracting URL parameters in the query string from the request to the authorization endpoint
-                // e.g., https://localhost:9443/oauth2/authorize?param1=value1&param2=value2 -> {param1=value1, param2=value2}
-                // Important: This code branch will most likely not get executed since there's no claims returned from the authorization endpoint.
-                requestWrapper = authAuthzReqMessageContext.getAuthorizationReqDTO().getHttpServletRequestWrapper();
-            } else {
-                log.error("Unable to extract request wrapper. Returning an empty map.");
-                return returnObject;
-            }
-
-            final String clientChannel = requestWrapper.getParameter(Constants.CLIENT_CHANNEL);
-            final String clientVersion = requestWrapper.getParameter(Constants.CLIENT_VERSION);
-
-            // Only add values if they are present
-            if (StringUtils.isNotBlank(clientChannel)) {
-                returnObject.put(Constants.CLIENT_CHANNEL, clientChannel);
-            } else {
-                log.warn("Client channel is null or empty. Not adding to JWT claim set.");
-            }
-
-            if (StringUtils.isNotBlank(clientVersion)) {
-                returnObject.put(Constants.CLIENT_VERSION, clientVersion);
-            } else {
-                log.warn("Client version is null or empty. Not adding to JWT claim set.");
-            }
-
-        } catch (Exception e) {
-            log.error("Error adding custom claims to JWT claim set. Returning default values only.", e);
-        }
+        returnObject.put(Constants.USER_CLAIM_1, "value1");
+        returnObject.put(Constants.USER_CLAIM_2, "value2");
 
         return returnObject;
     }
 
     private static final class Constants {
-        private static final String CLIENT_CHANNEL = "clientChannel";
-        private static final String CLIENT_VERSION = "clientVersion";
+        private static final String USER_CLAIM_1 = "userClaim1";
+        private static final String USER_CLAIM_2 = "userClaim2";
     }
 
 }
